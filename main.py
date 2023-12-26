668935738c4168f2bb5d1be463864bcae3055426
maor hornstein <mor.h100@gmail.com>
2023-12-26 09:20:40 +0200

    Add user state

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def home():
 @app.route('/chat', methods=['POST'])
 def chat():
     user_input = request.form['message']
-    if user_input.lower() == 'dummy input':
+    user_state = request.form['userState']  # Retrieve the user state from the form
+
+    # Logic to determine the server's reply based on user input and state
+    if user_input.lower() == 'dummy' and user_state == 'initialState':
         server_reply = 'Hi'
+        new_state = 'greeted'  # Update the state as needed
     else:
         server_reply = 'Bye'
-    return render_template('chat.html', user_input=user_input, server_reply=server_reply)
+        new_state = 'default'
+
+    # Include the updated state in the response
+    return render_template('chat.html', user_input=user_input, server_reply=server_reply, user_state=new_state)
 
 if __name__ == '__main__':
     app.run(debug=True)

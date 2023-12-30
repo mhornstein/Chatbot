5080293894e7b76f4c31b95998d0fb1508900447
maor hornstein <mor.h100@gmail.com>
2023-12-30 09:09:12 +0200

    reorganize constants

--- state_machine/constants.py
+++ state_machine/constants.py
@@ -1,9 +1,14 @@
+# input from user
 I_AM_BACK = 'חזרתי'
-INVALID_INPUT = 'אפשרות לא זמינה'
-DELIM = '\n\n'
 YES = 'כן'
 NO = 'לא'
+
+# cache keys
 NAME = 'name'
 REQUESTED_CAMERA = 'requested_camera'
 UNLOCKED_CAMERAS = 'unlocked_cameras'
+
+# agent output
+INVALID_INPUT = 'אפשרות לא זמינה'
+DELIM = '\n\n'
 COMMA_DELIM = ','
